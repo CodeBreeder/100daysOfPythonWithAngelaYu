c5bf4d53e6e8b2d6e7f4471461d12be323d5f233
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 01:40:21 -0500

    fix string output

---
+++ day-1/strings.py
@@ -0,0 +1,11 @@
+''' Update the following program so it prints exactly:
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')'''
+
+# Write your code below this line 👇🏾
+print("""
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+""")
