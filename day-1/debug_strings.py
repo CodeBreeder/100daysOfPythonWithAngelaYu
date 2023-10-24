29c0ebb51e96e32b5a56ac181b9b85ab0335bc1a
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 01:58:45 -0500

    update file name

---
+++ day-1/debug_strings.py
@@ -0,0 +1,35 @@
+''' 
+Debug the following commented codes so that they
+print exactly as they appear.
+'''
+
+
+# Statement 1:
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+# Write your code below this line 👇🏾
+
+print("""
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+""")
+
+# Statement 2:
+# print("Day 1 - Python Print Function")
+# Write your code below this line 👇🏾
+
+print('print("Day 1 - Python Print Function")')
+
+# Fix the code below 👇🏾
+
+# print("Day 1 - String Manipulation')
+# print("String Concatenation is done with the "+" sign.")
+#   print("e.g. print("Hello " + "world")')
+#       print(New lines can be created with a backslash and n.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
