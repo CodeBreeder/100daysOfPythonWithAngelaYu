2ac7888ea1e80671696c01d8414abcf32de624e2
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 03:29:30 -0500

    create band name generator

---
+++ day-2.py/data_type.py
@@ -0,0 +1,9 @@
+# Write a program that produces the sum of a two digits input() number
+
+two_digit_number = input()
+# 🚨 Don't change the code above ☝🏾
+####################################
+# Write your code below this line 👇🏾
+
+two_digit_number = int(two_digit_number[0]) + int(two_digit_number[1])
+print(two_digit_number)
